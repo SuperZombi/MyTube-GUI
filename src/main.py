c1d57c4a00f7dc3d35c970805abbf1f7ac66f493
Super Zombi <super.zombi.yt@gmail.com>
2026-01-03 13:27:34 +0000

    settings and update ytdlp

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ import traceback
 import socket
 
 
-__version__ = Version("1.1.0")
+__version__ = Version("2.0.0")
 @eel.expose
 def get_app_version(): return str(__version__)
 @eel.expose
--- src/web/scripts/settings.jsx
+++ src/web/scripts/settings.jsx
@@ -2,9 +2,31 @@ const Settings = ({
 	show, setShow, onReady
 }) => {
 	const [appVer, setAppVer] = React.useState("...")
+	const [newAppVer, setNewAppVer] = React.useState(null)
+	const [showUpdatePopup, setShowUpdatePopup] = React.useState(false)
 	const [ytDlp, setYtDlp] = React.useState("...")
-	const [SETTINGS, setSETTINGS] = React.useState(null)
-	const [settingsLoaded, setSettingsLoaded] = React.useState(false)
+	const [SETTINGS, setSETTINGS] = React.useState({})
+	const [updateAvalible, setUpdateAvalible] = React.useState(false)
+	React.useEffect(_=>{
+		(async _=>{
+			const SETTINGS = await eel.request_settings()()
+			setSETTINGS(SETTINGS)
+			if (SETTINGS.check_updates){
+				const avaliable = await eel.check_updates()()
+				if (avaliable){
+					setUpdateAvalible(true)
+					setNewAppVer(avaliable.new)
+					setShowUpdatePopup(true)
+				}
+			}
+			if (SETTINGS.theme){
+				applyTheme(SETTINGS.theme)
+			}
+			else if (SETTINGS.language){
+
+			}
+		})()
+	}, [])
 	React.useEffect(_=>{
 		(async _=>{
 			const app_ver = await eel.get_app_version()()
@@ -14,28 +36,42 @@ const Settings = ({
 			if (yt_dlp_ver){
 				setYtDlp(yt_dlp_ver)
 				onReady()
+				const SETTINGS = await eel.request_settings()()
+				if (SETTINGS.check_updates){
+					const fail = await eel.check_ytdlp_updates()()
+					if (fail.length == 0){
+						const new_version = await eel.get_yt_dlp_version()()
+						if (yt_dlp_ver != new_version){
+							console.log("yt_dlp_updated", new_version)
+						}
+					}
+				}
 			} else {
 				console.error("YT-DLP not found")
+
+				const fail = await eel.check_ytdlp_updates()()
+				if (fail.length > 0){
+					if (fail.includes("no_yt-dlp") && fail.includes("PermissionError")){
+						console.error("PermissionError")
+						// window.location.reload()
+					}
+					else if (fail.includes("no_yt-dlp")){
+						console.error("InternetError")
+						// window.location.reload()
+					}
+					else if (fail.includes("PermissionError")){
+						console.error("PermissionError")
+					}
+				} else {
+					const new_version = await eel.get_yt_dlp_version()()
+					if (new_version){
+						setYtDlp(new_version)
+						onReady()
+					}
+				}
 			}
-			// if (SETTINGS.check_updates){
-			// 	check_updates()
-			// 	check_ytdlp_updates(yt_dlp_ver)
-			// } else {
-			// 	if (!yt_dlp_ver){
-			// 		check_ytdlp_updates(yt_dlp_ver)
-			// 	}
-			// }
 		})()
 	}, [])
-	React.useEffect(_=>{
-		(async _=>{
-			if (!settingsLoaded){
-				const SETTINGS = await eel.request_settings()()
-				setSETTINGS(SETTINGS)
-				setSettingsLoaded(true)
-			}
-		})()
-	}, [settingsLoaded])
 
 	const changeSetting = async event=>{
 		const name = event.target.name
@@ -46,27 +82,37 @@ const Settings = ({
 		else if (name == "language"){
 
 		}
+		setSETTINGS(prev => ({
+			...prev,
+			[name]: value
+		}))
 		await eel.change_setting(name, value)()
-		setSettingsLoaded(false)
 	}
 	const requestOutputFolder = async _=>{
 		const answ = await eel.request_folder()()
 		if (answ){
 			await eel.change_setting("output_folder", answ)()
-			setSettingsLoaded(false)
+			setSETTINGS(prev => ({
+				...prev,
+				["output_folder"]: answ
+			}))
 		}
 	}
 	return (
+		<React.Fragment>
 		<Popup id="settings" show={show} setShow={setShow}>
 			<div className="content">
-				<h3>
-					<span lang_="version">Version</span>:
-					<span className="version" id="app_version">{appVer}</span>
-					<a id="update_avalible" href="https://github.com/SuperZombi/MyTube-GUI/releases" target="_blank"><i className="fa-solid fa-circle-up"></i></a>
+				<h3 className="versions">
+					<span>Version:</span>
+					<span className="version">{appVer}</span>
+					<a id="update_avalible" className={updateAvalible ? "show" : ""}
+						href="https://github.com/SuperZombi/MyTube-GUI/releases" target="_blank">
+						<i className="fa-solid fa-circle-up"></i>
+					</a>
 				</h3>
-				<h4>
-					<span>yt-dlp</span>:
-					<span className="version" id="yt_dlp_version">{ytDlp}</span>
+				<h4 className="versions">
+					<span>yt-dlp:</span>
+					<span className="version">{ytDlp}</span>
 				</h4>
 				<div className="grid">
 					<i className="fa-solid fa-circle-up"></i>
@@ -128,5 +174,18 @@ const Settings = ({
 				</div>
 			</div>
 		</Popup>
+		<Popup id="new-version-popup" show={showUpdatePopup} setShow={_=>setShowUpdatePopup(false)}>
+			<div className="content">
+				<div className="update-icon"><i className="fa-solid fa-circle-up"></i></div>
+				<span lang_="update_avalible">Update available!</span>
+				<div className="versions">
+					<span className="version">{appVer}</span>
+					<span>⮕</span>
+					<span className="version">{newAppVer}</span>
+				</div>
+				<a className="simple-button" href="https://github.com/SuperZombi/MyTube-GUI/releases" target="_blank" lang_="download_button">Download</a>
+			</div>
+		</Popup>
+		</React.Fragment>
 	)
 }
